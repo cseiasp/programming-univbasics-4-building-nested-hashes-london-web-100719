9119c4f2d7b53b8a84f23ffd39b1f69edd4ee303
Caroline S-A <caroline.seilern@hotmail.com>
2019-09-05 17:31:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    pieces: 4,
 	    rent_in_dollars: {
-	      key3: "value",
+	      one_piece_owned: "25",
 	      owner1: "value",
 	      key: "value",
 	      key2: "value"},
